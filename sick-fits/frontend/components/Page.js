c4bc00f6160d8e81b4fec01d9cae8df3d27cad05
Gautam Naik <gautamnaik1994@gmail.com>
2018-10-29 02:54:28 +0530

    added ui

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,36 +1,45 @@
-import React, {Component,Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
 import Header from './Header';
 import Meta from './Meta';
-import styled from 'styled-components';
+import styled, {ThemeProvider, injectGloabal} from 'styled-components';
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
 
 const StyledPage = styled.div`
-    background:white;
-    color:black;
+  background: white;
+  color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
-    max-width:1000px;
-    margin:0 auto;
-    padding:2rem;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
 `;
 
-class Page extends Component{
-    constructor(props){
-        super(props);
-    }
+class Page extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    render(){
-        return(
+  render() {
+    return (
+        <ThemeProvider  theme={theme}>
             <StyledPage>
-                <Meta/>
-                <Header />
-                <Inner>
-                     {this.props.children}
-                </Inner>
-               
+              <Meta />
+              <Header />
+              <Inner>{this.props.children}</Inner>
             </StyledPage>
-        )
-    }
+        </ThemeProvider>
+    );
+  }
 }
 
-export default Page
+export default Page;
